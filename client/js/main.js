c8546d20fd85a601f6413fff42e24a3fd4e42ab4
Cenk Yurtbilir <cyurtilir@nexient.com>
2015-06-04 09:12:56 -0400

    changed the indentation

--- client/js/main.js
+++ client/js/main.js
@@ -1,47 +1,47 @@
 'use strict';
 
-	var app = angular.module('PetFinder', ['ngMockE2E']);
+var app = angular.module('PetFinder', ['ngMockE2E']);
 
-	app.controller("DisplayController", function($scope, $http){
-		$http.get('/pets').success(function(response){
-			$scope.displayPets = response;
-		});
+app.controller("DisplayController", function($scope, $http){
+	$http.get('/pets').success(function(response){
+		$scope.displayPets = response;
 	});
+});
 
-	// define our fake backend
-	app.run(function($httpBackend) {
-	  var pets = [
-	  	{
-		  	sid: 24,
-		  	pid: 45,
-		  	name: 'Sophie',
-		  	sex: 'female',
-		  	animal: 'dog',
-		  	size: 'small',
-		  	breed: 'Yorkshire Terrior',
-		  	mix: 'None',
-		  	description: 'Sophie is a fun loving ball of cuteness',
-		  	age: 'adult',
-		  	status: 'available',
-		  	contact: '123-456-7890',
-		  	images: ['img/placeholderImage.svg']
-	  	},
-	  	{
-		  	sid: 62,
-		  	pid: 28,
-		  	name: 'Chester',
-		  	sex: 'male',
-		  	animal: 'cat',
-		  	size: 'large',
-		  	breed: 'Tabby',
-		  	mix: 'None',
-		  	description: 'Chester is crazy',
-		  	age: 'adult',
-		  	status: 'available',
-		  	contact: '123-456-7890',
-		  	images: ['img/placeholderImage.svg']
-	  	},
-	  	{
+// define our fake backend
+app.run(function($httpBackend) {
+  var pets = [
+  	{
+	  	sid: 24,
+	  	pid: 45,
+	  	name: 'Sophie',
+	  	sex: 'female',
+	  	animal: 'dog',
+	  	size: 'small',
+	  	breed: 'Yorkshire Terrior',
+	  	mix: 'None',
+	  	description: 'Sophie is a fun loving ball of cuteness',
+	  	age: 'adult',
+	  	status: 'available',
+	  	contact: '123-456-7890',
+	  	images: ['img/placeholderImage.svg']
+  	},
+  	{
+	  	sid: 62,
+	  	pid: 28,
+	  	name: 'Chester',
+	  	sex: 'male',
+	  	animal: 'cat',
+	  	size: 'large',
+	  	breed: 'Tabby',
+	  	mix: 'None',
+	  	description: 'Chester is crazy',
+	  	age: 'adult',
+	  	status: 'available',
+	  	contact: '123-456-7890',
+	  	images: ['img/placeholderImage.svg']
+  	},
+  	{
 			sid: 34,
 			id: 78,
 			name: 'Lion',
@@ -56,9 +56,9 @@
 			description: 'very loyal, but very dangerous too. Feeds with human',
 			images: ['img/placeholderImage.svg']
 		}
-	  ]; 
-	  
-	  $httpBackend.whenGET('/pets').respond(pets);
-	});
+  ]; 
+  
+  $httpBackend.whenGET('/pets').respond(pets);
+});
 	  
 	  
