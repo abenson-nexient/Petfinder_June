0e8e97dd8ea3730f1da23cc4436bd532b0f890db
Cenk Yurtbilir <cyurtilir@nexient.com>
2015-06-05 13:51:52 -0400

    solved the display button issue with jquery

--- client/js/controllers/user.controller.js
+++ client/js/controllers/user.controller.js
@@ -5,6 +5,9 @@ app.controller('UserController', function($scope, $http, StoreService) {
 	$scope.signInUser = function(user) {
 		$http.post('/user/signIn', user).success(function(response) {
 			$('#user-modal').modal('hide');
+			$('.navbar-right button').eq(0).hide();
+			$('.navbar-right button').eq(1).show();
+			$('.navbar-right button').eq(1).text('Hi, ' + response.username);
 		}).error(function(response) {
 			console.log(response);
 		});
