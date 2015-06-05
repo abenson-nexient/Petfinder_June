0aab0708d9037b836ad65257281228c1c95392f1
Cenk Yurtbilir <cyurtilir@nexient.com>
2015-06-05 13:44:01 -0400

    changed the index.html that now display controller will be used once

--- client/js/controllers/user.controller.js
+++ client/js/controllers/user.controller.js
@@ -1,10 +1,9 @@
 'use strict';
 
-app.controller('UserController', function($scope, $http) {
+app.controller('UserController', function($scope, $http, StoreService) {
 
 	$scope.signInUser = function(user) {
 		$http.post('/user/signIn', user).success(function(response) {
-			$scope.currentUsername = response.username;
 			$('#user-modal').modal('hide');
 		}).error(function(response) {
 			console.log(response);
