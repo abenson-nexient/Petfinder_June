25115b4e32eed7110a7230dbb333bd6c599fa4b3
Daniel Gill <dgill@nexient.com>
2015-06-09 12:31:43 -0400

    Cleanup PetController, add breed validation.

--- petfinder-app/src/main/java/com/nexient/petfinder/web/PetController.java
+++ petfinder-app/src/main/java/com/nexient/petfinder/web/PetController.java
@@ -1,10 +1,15 @@
 package com.nexient.petfinder.web;
 
-import java.beans.PropertyEditorSupport;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.petfinder.entity.PetAgeType;
 import org.petfinder.entity.PetGenderType;
@@ -12,15 +17,17 @@ import org.petfinder.entity.PetSizeType;
 import org.petfinder.entity.PetfinderPetRecord;
 import org.petfinder.entity.PetfinderPetRecordList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.nexient.petfinder.models.Pet;
 import com.nexient.petfinder.config.CaseInsensitiveConverter;
+import com.nexient.petfinder.models.Pet;
 import com.systemsinmotion.petrescue.entity.AnimalType;
 import com.systemsinmotion.petrescue.web.PetFinderConsumer;
 
@@ -30,7 +37,7 @@ public class PetController {
 
 	@Autowired
 	private PetFinderConsumer petFinderService;
-	
+
 	@RequestMapping("/random")
 	public Pet[] getRandomPet()
 	{
@@ -39,15 +46,43 @@ public class PetController {
 
 	@RequestMapping("/search")
 	public Pet[] searchPets(@RequestParam("location") String location,
-			@RequestParam(value="animal", required=false) AnimalType animalType,	// DOG, CAT, SMALL_FURRY, BARN_YARD, BIRD, HORSE, PIG, RABBIT, REPTILE
-			@RequestParam(value="breed", required=false) String[] breeds,       
-			@RequestParam(value="sex", required=false) PetGenderType sex,		// M, F
-			@RequestParam(value="age", required=false) PetAgeType[] ages, 		// BABY, YOUNG, ADULT, SENIOR
-			@RequestParam(value="size", required=false) PetSizeType[] sizes, 	// S, M, L, XL 
+			@RequestParam(value="animal", required=false) AnimalType animalType,		// DOG, CAT, SMALL_FURRY, BARN_YARD, BIRD, HORSE, PIG, RABBIT, REPTILE
+			@RequestParam(value="breed", required=false) String[] breeds,
+			@RequestParam(value="sex", required=false) PetGenderType sex,				// M, F
+			@RequestParam(value="age", required=false) PetAgeType[] ages, 				// BABY, YOUNG, ADULT, SENIOR
+			@RequestParam(value="size", required=false) PetSizeType[] sizes, 			// S, M, L, XL
 			@RequestParam(value="offset", required=false, defaultValue="0") int offset,
 			@RequestParam(value="count", required=false, defaultValue="30") int count) {
+		// Validate Location
+		if (location == null || location.isEmpty())
+			throw new IllegalArgumentException("The 'location' parameter must not be empty.");
 
-		breeds = breeds != null ? breeds : new String[] { null };
+		// Validate Breeds
+		if (breeds != null && breeds.length > 0) {
+			boolean breedsAreValid;
+			List<String> breedsAsList = Arrays.asList(breeds);
+			breedsAsList.replaceAll(breed -> breed.toLowerCase());
+			if (animalType != null) {
+				List<String> breedList = petFinderService.breedList(animalType.value().toLowerCase(), null).getBreed();
+				breedList.replaceAll(breed -> breed.toLowerCase());
+				breedsAreValid = breedList.containsAll(breedsAsList);
+			} else {
+				breedsAreValid = Arrays.stream(AnimalType.values())
+						.map(type -> type.value().toLowerCase())
+						.parallel()
+						.map(typeString -> petFinderService.breedList(typeString, null).getBreed())
+						.flatMap(list -> list.stream())
+						.map(breed -> breed.toLowerCase())
+						.collect(Collectors.toCollection(TreeSet::new))
+						.containsAll(breedsAsList);
+			}
+			if (!breedsAreValid)
+				throw new IllegalArgumentException("Invalid breeds given.");
+		} else {
+			breeds = new String[] { null };
+		}
+
+
 		ages = ages != null ? ages : new PetAgeType[] { null };
 		sizes = sizes != null ? sizes : new PetSizeType[] { null};
 		List<PetfinderPetRecordList> petLists = new LinkedList<PetfinderPetRecordList>();
@@ -74,7 +109,7 @@ public class PetController {
 				.limit(count)
 				.toArray(size -> new Pet[size]);
 	}
-	
+
 	@RequestMapping("/{id}")
 	public Pet[] getPedBySidPid(@PathVariable BigInteger id) {
 		PetfinderPetRecord ppr = petFinderService.readPet(id, "xml");
@@ -83,18 +118,22 @@ public class PetController {
 		else
 			return new Pet[] { Pet.fromPetFinderPetRecord(ppr) };
 	}
-	
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Petfinder!";
-    }
 
-    
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-    	binder.registerCustomEditor(PetAgeType.class, new CaseInsensitiveConverter<>(PetAgeType.class));
-    	binder.registerCustomEditor(PetGenderType.class, new CaseInsensitiveConverter<>(PetGenderType.class));
-    	binder.registerCustomEditor(PetSizeType.class, new CaseInsensitiveConverter<>(PetSizeType.class));
-    	binder.registerCustomEditor(AnimalType.class, new CaseInsensitiveConverter<>(AnimalType.class));
-    }
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Petfinder!";
+	}
+
+	@ExceptionHandler
+	void handleIllegalArgumentException(IllegalArgumentException e, HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.BAD_REQUEST.value());
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(PetAgeType.class, new CaseInsensitiveConverter<>(PetAgeType.class));
+		binder.registerCustomEditor(PetGenderType.class, new CaseInsensitiveConverter<>(PetGenderType.class));
+		binder.registerCustomEditor(PetSizeType.class, new CaseInsensitiveConverter<>(PetSizeType.class));
+		binder.registerCustomEditor(AnimalType.class, new CaseInsensitiveConverter<>(AnimalType.class));
+	}
 }
