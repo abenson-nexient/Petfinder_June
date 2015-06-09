4995488023e50216afdb19411d4984c2a1dfc81b
Cenk Yurtbilir <cyurtilir@nexient.com>
2015-06-09 16:38:15 -0400

    implemented search with the server

--- petfinder-app/resources/static/js/controllers/display.controller.js
+++ petfinder-app/resources/static/js/controllers/display.controller.js
@@ -6,8 +6,8 @@ angular.module('PetFinder').controller("DisplayController", function($scope, $ht
 		$scope.displayPets = response;
 	});
 
-	$scope.age = ['Baby', 'Young', 'Adult', 'Senior'];
-	$scope.sex = ['Male', 'Female', 'Unknown'];
+	$scope.ageOptions = ['Baby', 'Young', 'Adult', 'Senior'];
+	$scope.sexOptions = ['Male', 'Female', 'Unknown'];
  
 	$http.get(ServerUrl + '/meta/animals').success(function(response) {
 		$scope.animalTypes = response;
@@ -22,6 +22,18 @@ angular.module('PetFinder').controller("DisplayController", function($scope, $ht
 	$scope.getRandomPet = function() {
 		$http.get(ServerUrl + '/pet/random').success(function(response){		
 			StoreService.storeVal(response[0]);
+		});
+	};
+
+	$scope.searchPets = function(pet) {
+		var queryArray = [], query;
+		for(var i in pet)
+			queryArray.push(i + '=' + pet[i]);
+		query = queryArray.join('&');
+		$http.get(ServerUrl + '/pet/search?' + query).success(function(response) {
+			$scope.displayPets = response;
+		}).error(function(response) {
+			console.log(response);
 		});
 	};
 
