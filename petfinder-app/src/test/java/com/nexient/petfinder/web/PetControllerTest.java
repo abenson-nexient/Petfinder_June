dcefd79b117c5f1a45f235ac14f8222e8559a9c4
Daniel Gill <dgill@nexient.com>
2015-06-10 14:33:05 -0400

    Clean up PetControllerTest.

--- petfinder-app/src/test/java/com/nexient/petfinder/web/PetControllerTest.java
+++ petfinder-app/src/test/java/com/nexient/petfinder/web/PetControllerTest.java
@@ -1,16 +1,14 @@
 package com.nexient.petfinder.web;
 
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.isA;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.hamcrest.collection.IsArrayContaining;
-import org.hamcrest.generator.HamcrestFactoryWriter;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -34,21 +33,9 @@ public class PetControllerTest {
 
 	private MockMvc mockMvc;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
 	@Before
 	public void setUp() throws Exception {
 		mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-	}
-
-	@After
-	public void tearDown() throws Exception {
 	}
 
 	@Test
@@ -59,49 +46,27 @@ public class PetControllerTest {
 
 	@Test
 	public void testGetRandomPet() throws Exception {
-		String petElement = "$[*]['%s']";
-		
-		mockMvc.perform(get("/pet/random").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+		ResultActions result = mockMvc.perform(get("/pet/random").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
 		.andExpect(status().isOk())
 		.andExpect(content().contentType("application/json;charset=UTF-8"))
 		.andExpect(jsonPath("$").isArray())
 		.andExpect(jsonPath("$", hasSize(1))) // Only difference from testSearchpets.
-		.andExpect(jsonPath("$[*].name", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].animal", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].age", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].sex", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].status", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].size", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].mix", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].description", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].contact", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].id", everyItem(isA(Integer.class))))
-		.andExpect(jsonPath("$[*].breeds").isArray())
-		.andExpect(jsonPath("$[*].breeds[*]", everyItem(either(isA(String.class)).or(isEmptyOrNullString()))))
-		.andExpect(jsonPath("$[*].images").isArray())
-		.andExpect(jsonPath("$[*].images[*]", everyItem(either(isA(String.class)).or(isEmptyOrNullString()))));
+		.andExpect(jsonPath("$[*].id", everyItem(isA(Integer.class))));
+		
+		testPetStringFields(result);
+		testPetArrayFields(result);
 	}
 
 	@Test
 	public void testSearchPets() throws Exception {
-		mockMvc.perform(get("/pet/search?location=MI").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+		ResultActions result = mockMvc.perform(get("/pet/search?location=MI").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
 		.andExpect(status().isOk())
 		.andExpect(content().contentType("application/json;charset=UTF-8"))
 		.andExpect(jsonPath("$").isArray())
-		.andExpect(jsonPath("$[*].name", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].animal", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].age", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].sex", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].status", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].size", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].mix", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].description", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].contact", everyItem(isA(String.class))))
-		.andExpect(jsonPath("$[*].id", everyItem(isA(Integer.class))))
-		.andExpect(jsonPath("$[*].breeds").isArray())
-		.andExpect(jsonPath("$[*].breeds[*]", everyItem(either(isA(String.class)).or(isEmptyOrNullString()))))
-		.andExpect(jsonPath("$[*].images").isArray())
-		.andExpect(jsonPath("$[*].images[*]", everyItem(either(isA(String.class)).or(isEmptyOrNullString()))));
+		.andExpect(jsonPath("$[*].id", everyItem(isA(Integer.class))));
+		
+		testPetStringFields(result);
+		testPetArrayFields(result);
 	}
 
 	@Test
@@ -111,6 +76,33 @@ public class PetControllerTest {
 		.andExpect(status().isOk())
 		.andExpect(content().contentType("application/json;charset=UTF-8"))
 		.andExpect(content().json("[{\"images\": [\"http://photos.petfinder.com/photos/pets/32296622/1/?bust=1433004499&width=500&-x.jpg\"],\"breeds\": [\"Domestic Short Hair\"],\"name\": \"Pressley\",\"animal\": \"Cat\",\"age\": \"Baby\",\"sex\": \"Female\",\"status\": \"A\",\"size\": \"Medium\",\"mix\": \"yes\",\"description\": \"Primary Color: Brown Secondary Color: White Weight: 1.85 Age: 0yrs 0mths 10wks\",\"contact\": \"ilovethemutts@yahoo.com\",\"id\": 32296622}]"));
+	}
+
+	/**
+	 * Insure that all fields of a pet object array that should be arrays of Strings are in fact arrays of Strings.
+	 * @param result The result of a get request that should have returned an array of pet objects. 
+	 * @throws Exception result.andExpect can throw undefined exceptions.
+	 */
+	private void testPetArrayFields(ResultActions result) throws Exception {
+		String arrayField = "$[*]['%s']";
+		String arrayFieldItems = "$[*]['%s'][*]";
+		String[] arrayFields = new String[] { "images", "breeds" };
+		for (String name : arrayFields) {
+			result.andExpect(jsonPath(arrayField, name).isArray());
+			result.andExpect(jsonPath(arrayFieldItems, name).value(everyItem(isA(String.class))));
+		}
+	}
+	
+	/**
+	 * Insure that all fields of a pet object array that should be Strings are in fact Strings.
+	 * @param result The result of a get request that should have returned an array of pet objects. 
+	 * @throws Exception result.andExpect can throw undefined exceptions.
+	 */
+	private void testPetStringFields(ResultActions result) throws Exception {
+		String stringField = "$[*]['%s']";
+		String[] fields = new String[] { "name", "animal", "age", "sex", "status", "size", "mix", "description", "contact" };
+		for (String name : fields)
+			result.andExpect(jsonPath(stringField, name).value(everyItem(isA(String.class))));
 	}
 
 }
