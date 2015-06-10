06a6b5163991cc6931f57ed0d30eea7d490fa7a5
Cenk Yurtbilir <cyurtilir@nexient.com>
2015-06-10 11:13:44 -0400

    fixed a bug on the filter for petname

--- petfinder-app/resources/static/js/filters/petname.filter.js
+++ petfinder-app/resources/static/js/filters/petname.filter.js
@@ -2,7 +2,7 @@
 
 angular.module('PetFinder').filter('beautify', function() {
 	return function(data) {
-		var regexp = new RegExp(/\W+\D+/);
+		var regexp = new RegExp(/\s\W+\D+/);
 
 		if(regexp.test(data))
 			data = data.replace(regexp, '');
