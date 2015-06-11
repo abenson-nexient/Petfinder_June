7280da0922a06ff382e228c4c2547beb325878b3
Cenk Yurtbilir <cyurtilir@nexient.com>
2015-06-11 15:18:54 -0400

    updated the search service that user can search with the city parameters too

--- petfinder-app/resources/static/js/services/searchService.js
+++ petfinder-app/resources/static/js/services/searchService.js
@@ -4,8 +4,14 @@ angular.module('PetFinder').factory('SearchService', function() {
 	return {
 		genSearchQuery: function(params) {
 			var queryArray = [];
-			for(var i in params)
-				queryArray.push(i + '=' + params[i]);
+			for(var i in params) {
+				if(i === "location") {
+					var locationParams = params[i].split(' ').join('%20');
+					queryArray.push(i + '=' + locationParams);
+				} else {
+					queryArray.push(i + '=' + params[i]);
+				}
+			}
 			return queryArray.join('&');
 		}
 	}
