82f06bb1da6c3bb33909f94176c43112f6124888
Cenk Yurtbilir <cyurtilir@nexient.com>
2015-06-12 11:58:09 -0400

    removed annecesaary code from the search service

--- petfinder-app/resources/static/js/services/searchService.js
+++ petfinder-app/resources/static/js/services/searchService.js
@@ -6,12 +6,7 @@ angular.module('PetFinder').factory('SearchService', function() {
 			var queryArray = [];
 			for(var i in params) {
 				if(params[i] === null) continue;
-				if(i === "location") {
-					var locationParams = params[i].split(' ').join('%20');
-					queryArray.push(i + '=' + locationParams);
-				} else {
-					queryArray.push(i + '=' + params[i]);
-				}
+				queryArray.push(i + '=' + params[i]);
 			}
 			return queryArray.join('&');
 		}
