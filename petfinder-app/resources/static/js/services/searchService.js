88f920143f01f6981bf877942f21b0b94f431a84
Cenk Yurtbilir <cyurtilir@nexient.com>
2015-06-12 10:58:07 -0400

    fixed a bug on search

--- petfinder-app/resources/static/js/services/searchService.js
+++ petfinder-app/resources/static/js/services/searchService.js
@@ -5,6 +5,7 @@ angular.module('PetFinder').factory('SearchService', function() {
 		genSearchQuery: function(params) {
 			var queryArray = [];
 			for(var i in params) {
+				if(params[i] === null) continue;
 				if(i === "location") {
 					var locationParams = params[i].split(' ').join('%20');
 					queryArray.push(i + '=' + locationParams);
