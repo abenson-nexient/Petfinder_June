5ec35576a916e8245769f987f57433e2c2f3612e
Ian McPherson <ian.p.mcpherson@gmail.com>
2015-06-15 13:24:28 -0400

    Add spring dbcp dependencies, add application properties.

--- petfinder-app/src/main/java/com/nexient/petfinder/config/AppConfig.java
+++ petfinder-app/src/main/java/com/nexient/petfinder/config/AppConfig.java
@@ -1,17 +1,20 @@
 package com.nexient.petfinder.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  
 @EnableWebMvc
 @Configuration
 @ComponentScan({ "com.nexient.petfinder.web.*" })
+@EnableTransactionManagement
 @Import({ SecurityConfig.class })
 public class AppConfig {
  
 	// Beans go here.
- 
+	
 }
